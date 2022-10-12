86602bac62603482b2123b6712ee493b6a3b923e
Drlazinho <lazbonfim@hotmail.com>
2022-10-12 13:34:45 -0300

    commit: component TextInput stories

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     fontSize: {
       xs: 14,
@@ -10,26 +8,31 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      '2xl' : 32,
+      '2xl': 32
     },
     colors: {
+      transparent: 'transparent',
+
       black: '#000000',
       white: '#ffffff',
+      gray: {
+        900: '#09090A',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6'
+      },
 
-      'gray-900' : '#09090A',
-      'gray-800' : '#202024',
-      'gray-400' : '#7C7C8A',
-      'gray-200' : '#C4C4CC',
-      'gray-100' : '#E1E1E6',
-
-      'cyan-500' : '#61DAFB',
-      'cyan-300' : '#90E7FF',
+      cyan: {
+        500: '#61DAFB',
+        300: '#90E7FF'
+      }
     },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
-      },
-    },
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
